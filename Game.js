5f6126fb9eb56c2bbc47a8eec84b6548849cbb70
ALI736-WDS <ali.sh.p736@gmail.com>
2024-02-11 19:39:52 +0330

    Source Code JS Update 2023 added...

--- Game.js
+++ Game.js
@@ -1,44 +1,108 @@
-const choices = ["rock", "paper", "scissors"] ;
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
-const userChoice = prompt("Choose roch, paper, scissors") ;
-if(userChoice)
-	{
-		console.log(`You choose ${userChoice}`) ;
-	} else
-		{
-			console.log("You cheated") ;
-		}
+const checkWinner = (player, computer) => {
+	if (player === computer) {
+		return "draw";
+		// console.log("Its Tied");
+	} else if (player === "rock") {
+		// const res = computer === "scissors" ? "player" : "computer";
+		// return res;
+		//destructer
+		return computer === "scissors" ? "player" : "computer";
+	} else if (player === "paper") {
+		return computer === "rock" ? "player" : "computer";
+	} else {
+		return computer === "paper" ? "player" : "computer";
+	}
+}
 
-const randomNumber = Math.floor(Math.random() * 3) ; // tolid adad beine 0 ta 2: 0 1 2
-//console.log(randomNumber) ;
-const computerChoice = choices[randomNumber] ;
-console.log(`Computer chosses ${computerChoice}`) ;
+const showResult = (result) => {
+	if (result === "player") {
+		console.log("You Win");
+		playerScore++;
+	}
+	else if (result === "computer") {
+		console.log("You lose");
+		computerScore++;
+	} else {
+		console.log("it's tie");
+	}
 
-if(userChoice === computerChoice)
-	{
-		console.log("it's a tie") ;
-	}else if(userChoice === "rock")
-		{
-			if(computerChoice === "scissors")
-			{
-			console.log("You win!") ;
-			}else{
-			console.log("You lose!") ;
-		}
-		}else if(userChoice === "paper")
-			{
-				if(computerChoice === "rock")
-					{
-						console.log("You win!") ;
-					}else{
-						console.log("You lose!") ;
-					}
-			}else if(userChoice === "scissors")
-				{
-					if(computerChoice === "paper")
-						{
-							console.log("You win!") ;
-						}else{
-							console.log("You lose!")
-						}
-				}
+	console.log(`Your Score ${playerScore}`);
+	console.log(`Computer's Score ${computerScore}`);
+	console.log(`........................`);
+}
+
+const play = () => {
+	const playerChoice = prompt("Choose rock, paper, scissors");
+	// if (playerChoice === "paper" ||
+	// 	playerChoice === "rock" ||
+	// 	playerChoice === "scissors"
+	// ) {
+
+	// bekhatere toLowerCase agar cancel bezanim error mide
+	//shart typeof bara in gozashte shode ke agar cancel ro zadim error nade va "You cheated" ejra beshe
+	// if (choices.indexOf(typeof playerChoice === "string" && playerChoice.toLowerCase()) !== -1) {  // !== -1 : yani faghat index 0,1,2
+	//agar bekhaim code sade tar beshe az optional chaning estefade mikonim(?) ke agar playerChoice undefine budT baghie ham undefine beshe
+	if (choices.indexOf(playerChoice) !== -1) {  // !== -1 : yani faghat index 0,1,2
+		console.log(`You choose ${playerChoice}`);
+	} else {
+		console.log("You cheated");
+		return;
+	}
+
+	// const randomNumber = Math.floor(Math.random() * 3); // tolid adad beine 0 ta 2: 0 1 2
+	const randomNumber = Math.floor(Math.random() * choices.length); // tolid adad beine 0 ta 2: 0 1 2
+	//console.log(randomNumber) ;
+	const computerChoice = choices[randomNumber];
+	console.log(`Computer choses ${computerChoice}`);
+
+	const gameResult = checkWinner(playerChoice, computerChoice);
+	showResult(gameResult);
+	play();
+
+	// if (playerScore === 5) {
+	// 	console.log("Player is Win");
+	// 	console.log("Finish Game");
+	// }
+	// if (computerScore === 5) {
+	// 	console.log("Computer is Win");
+	// 	console.log("Finish Game");
+	// }
+	//ke bad az etmam bazi dobare ejra beshe (poshte sare ham)
+};
+
+play();
+
+
+
+// if (player === computer) {
+// 	return "draw";
+// 	// console.log("Its Tied");
+// } else if (player === "rock") {
+// 	if (computer === "scissors") {
+// 		return "Player win.";
+// 		// console.log("Player win.")
+// 	} else {
+// 		return "Computer win.";
+// 		// console.log("Computer win.")
+// 	}
+// } else if (player === "paper") {
+// 	if (computer === "rock") {
+// 		return "Player win.";
+// 		// console.log("Player win.")
+// 	} else {
+// 		return "Computer win.";
+// 		// console.log("Computer win.")
+// 	}
+// } else if (player === "scissors") {
+// 	if (computer === "paper") {
+// 		return "Player win.";
+// 		// console.log("Player win.")
+// 	} else {
+// 		return "Computer win.";
+// 		// console.log("Computer win.")
+// 	}
+// }
